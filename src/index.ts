fbb630be107729086133071b542b1e2fd2710f1e
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-03 19:24:54 +0600

    function

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,40 @@
 /**
  * Debug
  */
-// let age: number = 26;
+let age: number = 26;
 
-// if (age < 50) age += 10;
-// console.log(age);
+if (age < 50) age += 10;
+console.log(age);
 
 /**
  * Any
  */
-// let sales: number = 123_456_789;
-// let course = "TypeScript";
-// let is_published = true;
+let sales: number = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
 
-// let level;
+let level;
 
-// function test(document) {
-//   console.log(document);
-// }
+function test(document) {
+  console.log(document);
+}
 
 /**
  * Array
  */
-// let num01 = [1, 2, "3"]; // valid code
-// let numbers: number[] = [1, 2, "3"]; // now throw error
+let num01 = [1, 2, "3"]; // valid code
+let numbers: number[] = [1, 2, "3"]; // now throw error
 
-// let num2: number[] = [];
-// // num2[0] = 1;
-// // num2[1] = "1";
-// num2.forEach((n) => n);
+let num2: number[] = [];
+// num2[0] = 1;
+// num2[1] = "1";
+num2.forEach((n) => n);
 
 /**
  * Tuples
  */
 
-// let user: [number, string, boolean, number] = [1, "Akash", true, 0];
+let user: [number, string, boolean, number] = [1, "Akash", true, 0];
 
 /**
  * Enums
@@ -51,18 +51,23 @@
 //     Large,
 //   }
 // If we use `const`, it generates more optimize code
-// const enum Size {
-//   Small = 5,
-//   Medium,
-//   Large,
-// }
-// let mySize: Size = Size.Large;
-// console.log(mySize);
+const enum Size {
+  Small = 5,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Large;
+console.log(mySize);
 
 /**
  * Functions
  */
 
 function calculateTax(income: number): number {
-  return 0;
+  return income;
+}
+
+// Use ? for optional parameter
+function calculateTax1(income: number, deposit?: number): number {
+  return income - (deposit || 20_000);
 }
