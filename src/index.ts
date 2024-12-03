7a6db124f3656302d55e6ba5cda7c8411090e1c5
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-03 10:03:58 +0600

    array

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-// let age: number = 26;
+/**
+ * Debug
+ */
+let age: number = 26;
 
-// if (age < 50) age += 10;
-// console.log(age);
+if (age < 50) age += 10;
+console.log(age);
 
+/**
+ * Any
+ */
 let sales: number = 123_456_789;
-let course: string = "TypeScript";
-let is_published: boolean = true;
+let course = "TypeScript";
+let is_published = true;
 
 let level;
 
 function test(document) {
   console.log(document);
 }
+
+/**
+ * Array
+ */
+let num01 = [1, 2, "3"]; // valid code
+let numbers: number[] = [1, 2, "3"]; // now throw error
+
+let num2: number[] = [];
+// num2[0] = 1;
+// num2[1] = "1";
+num2.forEach((n) => n);
