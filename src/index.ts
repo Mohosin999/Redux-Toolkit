ead735d5ceee851a9d5908303adb3092281b6a39
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-03 09:25:01 +0600

    any types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-let age: number = 26;
+// let age: number = 26;
 
-if (age < 50) age += 10;
-console.log(age);
+// if (age < 50) age += 10;
+// console.log(age);
+
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+let level;
+
+function test(document) {
+  console.log(document);
+}
